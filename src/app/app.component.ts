98d62c5ddca68d53ed8d93cec689b899cc5c2195
waldinho <james.waller1984@gmail.com>
2023-06-29 11:31:13 +1000

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'angular-attribute-directive';
+  color = '';
+  defaultColor = 'violet';
+  warningMessage = '';
+  setWarningMessage() {
+    this.color === ''
+      ? (this.warningMessage = `Choose your highlight color fool! ${this.defaultColor} doesn't count`)
+      : (this.warningMessage = '');
+  }
+}
